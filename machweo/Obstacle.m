c0774c5a7993a1cb1eba6693145c7b7e81dcb5d7
John Feldcamp <johnfeldcamp@ns1.cookinrelaxin.net>
2015-01-09 03:42:38 -0500

    just a new test level. its fucking hard

--- machweo/Obstacle.m
+++ machweo/Obstacle.m
@@ -11,20 +11,7 @@
 @implementation Obstacle
 
 +(instancetype)obstacleWithTextureAndPhysicsBody:(SKTexture *)texture{
-   // CGVector scaleCoefficient = [Constants sharedInstance].SCALE_COEFFICIENT;
-    //texture.size = CGSizeMake(0, 0);
-    //texture.size = CGSizeMake(texture.size.width * scaleCoefficient.dy, texture.size.height * scaleCoefficient.dy);
     Obstacle* obstacle = [Obstacle spriteNodeWithTexture:texture];
-//    obstacle.physicsBody = [SKPhysicsBody bodyWithTexture:texture size:texture.size];
-//    obstacle.physicsBody.categoryBitMask = [Constants sharedInstance].OBSTACLE_HIT_CATEGORY;
-//    obstacle.physicsBody.contactTestBitMask = [Constants sharedInstance].PLAYER_HIT_CATEGORY;
-//    //obstacle.physicsBody.collisionBitMask = 3;
-//    
-//   // NSLog(@"obstacle.physicsBody.categoryBitMask: %d", obstacle.physicsBody.categoryBitMask);
-//   // NSLog(@"obstacle.physicsBody.contactTestBitMask: %d", obstacle.physicsBody.contactTestBitMask);
-//    
-//    obstacle.physicsBody.dynamic = false;
-    
     return obstacle;
 }
 
