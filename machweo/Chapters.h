fe1c139175ac5adc76adafc48e72eaf4272360da
Zachary Satoshi Feldcamp <zfeldcam@umail.iu.edu>
2015-01-20 20:34:54 -0500

    high scores and persistence initial commit

---
+++ machweo/Chapters.h
@@ -0,0 +1,13 @@
+//
+//  Chapters.h
+//  machweo
+//
+//  Created by Feldcamp, Zachary Satoshi on 1/20/15.
+//  Copyright (c) 2015 Zachary Feldcamp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Chapters : NSObject
+
+@end
---
+++ machweo/Chapters.m
@@ -0,0 +1,13 @@
+//
+//  Chapters.m
+//  machweo
+//
+//  Created by Feldcamp, Zachary Satoshi on 1/20/15.
+//  Copyright (c) 2015 Zachary Feldcamp. All rights reserved.
+//
+
+#import "Chapters.h"
+
+@implementation Chapters
+
+@end
